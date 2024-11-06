9da0bb6c00b7ac7f5fdd976939d4ecb8091a63f2
Efrain May <58404444+efraindrummer@users.noreply.github.com>
2024-11-06 09:43:24 -0600

    Update example_datagrid.vb

--- example_datagrid.vb
+++ example_datagrid.vb
@@ -21,3 +21,27 @@ Private Sub Form_Load()
     ' Enlazar el recordset al DataGrid
     Set DataGrid1.DataSource = rs
 End Sub
+' EJEMPLO CON CLICK EVENT
+Private Sub cmdCargarDatos_Click()
+    Dim conn As ADODB.Connection
+    Dim rs As ADODB.Recordset
+    Dim connString As String
+    Dim query As String
+    
+    ' Definir la cadena de conexión
+    connString = "Provider=SQLOLEDB;Data Source=nombre_servidor;Initial Catalog=nombre_bd;User ID=usuario;Password=contraseña;"
+    
+    ' Definir la consulta SQL
+    query = "SELECT * FROM nombre_tabla"
+    
+    ' Crear y abrir la conexión
+    Set conn = New ADODB.Connection
+    conn.Open connString
+    
+    ' Crear y abrir el recordset
+    Set rs = New ADODB.Recordset
+    rs.Open query, conn, adOpenStatic, adLockReadOnly
+    
+    ' Enlazar el recordset al DataGrid
+    Set DataGrid1.DataSource = rs
+End Sub
